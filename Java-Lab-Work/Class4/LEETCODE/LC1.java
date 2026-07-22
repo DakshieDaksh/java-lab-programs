f8faa7e6059d1281b62b83e95dc08bc80179999e
Daksh Singh <113781725+DakshieDaksh@users.noreply.github.com>
2026-07-22 16:02:10 +0530

    Class 4 over with leetcode

--- Java-Lab-Work/Class4/LEETCODE/LC1.java
+++ Java-Lab-Work/Class4/LEETCODE/LC1.java
@@ -56,20 +56,24 @@ class Solution {
 
  */
 
-
-
-
 class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
-
-
-    }
-
-
+  public ListNode mergeTwoLists(ListNode list1, ListNode list2) {}
 }
 
-
 public class LC1 {
-    public s
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+    ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+    ListNode mergedList = solution.mergeTwoLists(list1, list2);
+
+    // Print the merged list
+    while (mergedList != null) {
+      System.out.print(mergedList.val + " ");
+      mergedList = mergedList.next;
+    }
+  }
 }
